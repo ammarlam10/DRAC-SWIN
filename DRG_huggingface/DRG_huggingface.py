029c6187ff91ce4a551e6655e8d1875d7a6eb9c8
Ammar <ammarlam10@gmail.com>
2022-09-11 18:25:43 +0200

    swin

--- DRG_huggingface/DRG_huggingface.py
+++ DRG_huggingface/DRG_huggingface.py
@@ -108,7 +108,7 @@ class DRG(datasets.GeneratorBasedBuilder):
             
 
             df= pd.read_csv(csv_path)
-            print(df.shape[0])
+            print(df.shape[1])
             for k,v in df.iloc[:-100].iterrows():
             #for k,v in df.iterrows():
                 # print(v['image name'])
